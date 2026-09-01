c528609a72348a33dc37e4e4db471ab8816131f1
giancastillo732-create <giancastillo732@gmail.com>
2026-09-01 21:50:38 +0800

    initial

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+
+        String firstname = "Carlo Gian"; // Stores the first name
+        String lastname = "Castillo"; // Stores the last name
+        String address = "Guiset Sur, San Manuel"; // Stores the address
+        String mobileNumber = "09638107629"; // Stores the mobile number
+        int age = 17; // Stores the age
+        double gwa = 95.53; // Stores the GWA
+
+        System.out.println("Firstname: " + firstname); // Displays the first name
+        System.out.println("Lastname: " + lastname); // Displays the last name
+        System.out.println("Address: " + address); // Displays the address
+        System.out.println("Mobile Number: " + mobileNumber); // Displays the mobile number
+        System.out.println("Age: " + age); // Displays the age
+        System.out.println("GWA: " + gwa); // Displays the GWA
+    }
+}
